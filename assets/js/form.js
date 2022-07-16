e90f6acb7c5a72e7d1ca5f4d67677f97b1f75bc0
caique <caique.log@gmail.com>
2022-07-16 11:27:23 -0300

    => Versão com comentarios! <= card do form.html sendo construido com js a partir do id enviado ao clicar em avaliar

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,5 @@
-// console.log(page)
+let formItemId = localStorage.getItem("id");
+
+document.querySelector('.card img').src = moviesJson[formItemId].img;
+document.querySelector('.card img').alt = moviesJson[formItemId].title;
+document.querySelector('.card h3').innerHTML = moviesJson[formItemId].title;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 // initial data
-let page = 'id-1'; // pagina atual
-let currentPage = parseInt(page.slice(3)); // numero da pagina atual
+let section = 'page-1'; // pagina atual
+let currentPage = parseInt(section.slice(5)); // numero da pagina atual
 let pageItem = document.querySelectorAll('.page-item'); // array com os elementos da paginação
 let pageEnum = document.querySelectorAll('.pageEnum'); // array com os numeros da paginação
 let maxCards = 12; // quantidade de cards exibidos por pagina
+let itemId = '';
 
 // events
 printCard(); // imprime os cards na tela
@@ -17,35 +18,43 @@ function printCard() {
         let clonesQuantity = document.querySelectorAll('.clone').length; // conta a quantidade de clones criados
 
         // se o id for o mesmo da pagina e a quantidade de clones criados for menor que o maximo da pagina
-        if(item.id == page && clonesQuantity < maxCards ) { // verifica se o item no objeto pertence a pagina e delimita a quantidade de impressão
+        if(item.page == section && clonesQuantity < maxCards ) { // verifica se o item no objeto pertence a pagina e delimita a quantidade de impressão
             document.querySelector('.row').append(cardItem); // insere o clone do card no html
             // insere as informações dos cards clonados
             cardItem.classList.add('clone'); // insere a classe que identifica como clone
-            cardItem.classList.add(`${item.id}`); // insere o id que identifica o numero da pagina a que pertence
+            cardItem.classList.add(`${item.page}`); // insere o id que identifica o numero da pagina a que pertence
             cardItem.querySelector('h3').innerHTML = item.title; // insere o titulo 
             cardItem.querySelector('.card img').src = item.img; // insere a img 
             cardItem.querySelector('.card img').alt = item.title; // insere o texto alternativo da img
+            cardItem.querySelector('.clone .btn').setAttribute('data-index', item.id); // insere o id do objeto p/ uso no form.html
         }         
     });
+    let cloneBtns = document.querySelectorAll('.clone .btn'); // array de botoes avaliar
+    // ao clicar em avaliar insere o id do filme do card clicado no html e salva no navegador p/ usar no form.html
+    cloneBtns.forEach(e => e.addEventListener('click', () => { // p/ cada item adiciona o evento de click
+        itemId =  e.getAttribute('data-index'); // salva em itemId o id do objeto do card clonado
+        localStorage.setItem("id", itemId); // salva localmente no navegador o itemId
+    }));
+    
 }
 
 function pagination() {
    // no array de itens da paginação adiciona evento de click em cada item 
     pageItem.forEach(e => e.addEventListener('click', () => { 
         if(e.id == 'next' && currentPage < pageEnum.length) { // se ao clicar no botao next não for a ultima pagina
-            page = `id-${currentPage += 1}`; // altera o numero da pagina para a proxima
+            section = `page-${currentPage += 1}`; // altera o numero da pagina para a proxima
             
             activeClass(pageEnum, pageItem[currentPage]); // muda a classe active do elemento atual para o proximo
 
         } else if (e.id == 'prev' && currentPage > 1) { // se ao clicar no botao anterior não for a primeira pagina
-           page = `id-${currentPage -= 1}`; // altera o numero da pagina para a proxima
+           section = `page-${currentPage -= 1}`; // altera o numero da pagina para a proxima
 
            activeClass(pageEnum, pageItem[currentPage]); // muda a classe active do elemento atual para o proximo
 
         } else if(e.classList.contains('pageEnum')) { // se ao clicar no item, ele for um dos numeros
             // pagina atual é igual ao texto dentro da tag ancora dentro do item atual, convertido em inteiro
             currentPage = parseInt(e.querySelector('a').innerText); 
-            page = `id-${currentPage}`; // altera o numero da pagina de acordo com o numero clicado
+            section = `page-${currentPage}`; // altera o numero da pagina de acordo com o numero clicado
 
             activeClass(pageEnum, e); // // muda a classe active do elemento atual para o clicado
         }
@@ -81,9 +90,12 @@ function pagination() {
         let clones = document.querySelectorAll('.clone');
 
         clones.forEach(e => { // p cada item no array de clones
-            if(!e.classList.contains(`${page}`)) { // se o item não contem a classe pagina(se ele não pertence a pagina)
+            if(!e.classList.contains(`${section}`)) { // se o item não contem a classe pagina(se ele não pertence a pagina)
                 e.remove(); // remova-o
             }
         });
     }
 }
+
+
+
