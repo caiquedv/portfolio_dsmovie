ee1a927657d56c62b34f959ae494efa3965170e9
caique <caique.log@gmail.com>
2022-07-16 14:57:08 -0300

    Feito validação nativa do form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,74 @@
-let formItemId = localStorage.getItem("id");
+// Initial data
+let movieId = localStorage.getItem("id");
+let form = document.querySelector('form');
+let emailInput = form.querySelector('input');
+let i;
 
-document.querySelector('.card img').src = moviesJson[formItemId].img;
-document.querySelector('.card img').alt = moviesJson[formItemId].title;
-document.querySelector('.card h3').innerHTML = moviesJson[formItemId].title;
+// Events
+    // insere as informações no card
+document.querySelector('.card img').src = moviesJson[movieId].img;
+document.querySelector('.card img').alt = moviesJson[movieId].title;
+document.querySelector('.card h3').innerHTML = moviesJson[movieId].title;
+    
+form.addEventListener('submit', (event) => { // Validação nativa do formulário
+    event.preventDefault();
+    clearErrors(); 
+
+    let send = true;
+    let check = checkInput(); 
+   
+    if(check !== true) { 
+        send = false;
+        showError(emailInput, check); 
+    }
+    
+    if(send) {
+        form.submit();
+    }
+});
+
+// functions
+function checkInput() { //verifica se o input foi preenchico corretamente
+    let rules = emailInput.getAttribute('data-rules').split('|');
+    
+    for(i in rules) {
+        switch(rules[i]) {
+            case 'required':
+                if(emailInput.value == '') {
+                    return 'Campo não pode ser vazio!';
+                }
+            break;
+
+            case 'email':
+                if(emailInput.value != '') {
+                    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    
+                    if(!regex.test(emailInput.value.toLowerCase())) {
+                        return 'E-mail digitado não é válido!';
+                    }
+                }
+            break;
+        }
+    }
+
+    return true;
+}
+
+function showError(input, error) { 
+    input.style.borderColor = '#FF0000';
+
+    let errorElement = document.createElement('div');
+
+    errorElement.classList.add('error');
+    errorElement.innerHTML = error;
+    input.parentElement.insertBefore(errorElement, input.ElementSibling);
+}
+
+function clearErrors() { 
+    let errorElements = document.querySelectorAll('.error');
+    let errorQuantity = errorElements.length;
+    
+    for(i = 0 ; i < errorQuantity; i++) {
+        errorElements[i].remove();
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,90 @@
 // initial data
-let section = 'page-1'; // pagina atual
-let currentPage = parseInt(section.slice(5)); // numero da pagina atual
-let pageItem = document.querySelectorAll('.page-item'); // array com os elementos da paginação
-let pageEnum = document.querySelectorAll('.pageEnum'); // array com os numeros da paginação
-let maxCards = 12; // quantidade de cards exibidos por pagina
-let itemId = '';
+let cardPage = 'page-1'; 
+let currentPage = parseInt(cardPage.slice(5)); 
+let pageBtns = document.querySelectorAll('.page-item'); 
+let pageEnuns = document.querySelectorAll('.pageEnum'); 
+let pageQuantity = pageEnuns.length;
+let maxCard = 12; 
 
 // events
 printCard(); // imprime os cards na tela
-pagination(); // ativa a navegação da pagina
+pagination(); // controla a paginação
 
 // functions
 function printCard() {
-    // p/ cada item no objeto
-    moviesJson.map((item) => {
-        let cardItem = document.querySelector('.cardItem').cloneNode(true); // cria modelo de card clonado
-        let clonesQuantity = document.querySelectorAll('.clone').length; // conta a quantidade de clones criados
+    moviesJson.map((movie) => {
+        let cloneCard = document.querySelector('.cardItem').cloneNode(true); 
+        let clonesQuantity = document.querySelectorAll('.clone').length; 
+        let cardImg = cloneCard.querySelector('.card img');
+        let rateBtn = cloneCard.querySelector('.btn');
+        let cardTitle = cloneCard.querySelector('h3');
 
-        // se o id for o mesmo da pagina e a quantidade de clones criados for menor que o maximo da pagina
-        if(item.page == section && clonesQuantity < maxCards ) { // verifica se o item no objeto pertence a pagina e delimita a quantidade de impressão
-            document.querySelector('.row').append(cardItem); // insere o clone do card no html
+        // verifica se o item no objeto pertence a pagina e delimita a quantidade de impressão
+        if(movie.page == cardPage && clonesQuantity < maxCard ) { 
+            document.querySelector('.row').append(cloneCard); 
+
             // insere as informações dos cards clonados
-            cardItem.classList.add('clone'); // insere a classe que identifica como clone
-            cardItem.classList.add(`${item.page}`); // insere o id que identifica o numero da pagina a que pertence
-            cardItem.querySelector('h3').innerHTML = item.title; // insere o titulo 
-            cardItem.querySelector('.card img').src = item.img; // insere a img 
-            cardItem.querySelector('.card img').alt = item.title; // insere o texto alternativo da img
-            cardItem.querySelector('.clone .btn').setAttribute('data-index', item.id); // insere o id do objeto p/ uso no form.html
+            cloneCard.classList.add('clone'); 
+            cloneCard.classList.add(`${movie.page}`);
+            cardTitle.innerHTML = movie.title; 
+            cardImg.src = movie.img; 
+            cardImg.alt = movie.title; 
+            rateBtn.setAttribute('data-movie-id', movie.id);
+            rateBtn.addEventListener('click', () => { 
+                localStorage.setItem("id", rateBtn.getAttribute('data-movie-id')); // salva no navegador o id do card avaliado p/ construção do form
+            });
         }         
-    });
-    let cloneBtns = document.querySelectorAll('.clone .btn'); // array de botoes avaliar
-    // ao clicar em avaliar insere o id do filme do card clicado no html e salva no navegador p/ usar no form.html
-    cloneBtns.forEach(e => e.addEventListener('click', () => { // p/ cada item adiciona o evento de click
-        itemId =  e.getAttribute('data-index'); // salva em itemId o id do objeto do card clonado
-        localStorage.setItem("id", itemId); // salva localmente no navegador o itemId
-    }));
-    
+    });    
 }
 
-function pagination() {
-   // no array de itens da paginação adiciona evento de click em cada item 
-    pageItem.forEach(e => e.addEventListener('click', () => { 
-        if(e.id == 'next' && currentPage < pageEnum.length) { // se ao clicar no botao next não for a ultima pagina
-            section = `page-${currentPage += 1}`; // altera o numero da pagina para a proxima
+function pagination() { 
+    // verifica qual botão clicado e sua ação
+    pageBtns.forEach(btn => btn.addEventListener('click', () => { 
+        if(btn.id == 'next' && currentPage < pageQuantity) { 
+            cardPage = `page-${currentPage += 1}`; 
             
-            activeClass(pageEnum, pageItem[currentPage]); // muda a classe active do elemento atual para o proximo
+            activeClass(pageEnuns, pageBtns[currentPage]); 
 
-        } else if (e.id == 'prev' && currentPage > 1) { // se ao clicar no botao anterior não for a primeira pagina
-           section = `page-${currentPage -= 1}`; // altera o numero da pagina para a proxima
+        } else if (btn.id == 'prev' && currentPage > 1) {
+           cardPage = `page-${currentPage -= 1}`; 
 
-           activeClass(pageEnum, pageItem[currentPage]); // muda a classe active do elemento atual para o proximo
+           activeClass(pageEnuns, pageBtns[currentPage]); 
 
-        } else if(e.classList.contains('pageEnum')) { // se ao clicar no item, ele for um dos numeros
-            // pagina atual é igual ao texto dentro da tag ancora dentro do item atual, convertido em inteiro
-            currentPage = parseInt(e.querySelector('a').innerText); 
-            section = `page-${currentPage}`; // altera o numero da pagina de acordo com o numero clicado
+        } else if(btn.classList.contains('pageEnum')) { 
+            currentPage = parseInt(btn.querySelector('a').innerText); 
+            cardPage = `page-${currentPage}`; 
 
-            activeClass(pageEnum, e); // // muda a classe active do elemento atual para o clicado
+            activeClass(pageEnuns, btn); 
         }
 
       
-        disabledClass(); // insere a classe disabled em next e prev
-        eraseCard(); // apaga os cards clonados
-        printCard(); // imprime os novos cards clonados
+        disabledClass(); // desabilita next e prev ao chegar na ultima e primeira paginas
+        eraseCard(); // apaga os cards clonados ao trocar de pagina
+        printCard(); 
     }));
 
-    function activeClass(itemRemove, itemAdd) { 
-        itemRemove.forEach(e => { // p cada elemento no array de pagição
-            e.classList.remove('active'); // remove a classe active
-            e.ariaCurrent = ''; // remove aria-current
+    function activeClass(allBtns, clickedBtn) { // controla item ativo
+        allBtns.forEach(btn => { 
+            btn.classList.remove('active'); 
+            btn.ariaCurrent = ''; 
         });
 
-        itemAdd.classList.add('active'); // no elemento atual insere a classe active
-        itemAdd.ariaCurrent = 'page' // no elemento atual insere aria-current
+        clickedBtn.classList.add('active'); 
+        clickedBtn.ariaCurrent = 'page'
     }
 
     function disabledClass() {
-        let prev =  document.getElementById('prev') // botao proximo
-        let next =  document.getElementById('next') // botao anterior
+        let prev =  document.getElementById('prev') 
+        let next =  document.getElementById('next') 
 
-        //remove e adiciona disabled nos botoes next e prev quando entra e sai da primeira e ultima pagina
-        // se não for a primeira pagina -> remova disabled -> se for adicione disabled
         currentPage > 1 ? prev.classList.remove('disabled') : prev.classList.add('disabled');
-        // se não for a ultima pagina -> remova disabled -> se for adicione disabled
         currentPage < 3 ? next.classList.remove('disabled') : next.classList.add('disabled');
     }
 
     function eraseCard() {
-        let clones = document.querySelectorAll('.clone');
-
-        clones.forEach(e => { // p cada item no array de clones
-            if(!e.classList.contains(`${section}`)) { // se o item não contem a classe pagina(se ele não pertence a pagina)
-                e.remove(); // remova-o
+        document.querySelectorAll('.clone').forEach(cloneCard => { 
+            if(!cloneCard.classList.contains(`${cardPage}`)) { 
+                cloneCard.remove(); 
             }
         });
     }
