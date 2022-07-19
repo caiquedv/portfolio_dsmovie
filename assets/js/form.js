76de2224d66cf17985899ca21b1cd25e864f9a27
caique <caique.log@gmail.com>
2022-07-19 20:57:21 -0300

    cancelBtn fecha a pagina de avaliação e refatorado nativeValidation

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,69 +1,72 @@
 // Initial data
 let movieId = sessionStorage.getItem("id");
+let cardImg = document.querySelector('.card img');
+let cardTitle = document.querySelector('.card h3');
 let form = document.querySelector('form');
 let emailInput = form.querySelector('input');
 let voteInput = form.querySelector('select');
+let cancelBtn = document.querySelector('a.btn');
 let i;
 
 // Events
-    // insere as informações no card
-document.querySelector('.card img').src = moviesJson[movieId].img;
-document.querySelector('.card img').alt = moviesJson[movieId].title;
-document.querySelector('.card h3').innerHTML = moviesJson[movieId].title;
+// insere as informações no card
+cardImg.src = moviesJson[movieId].img;
+cardImg.alt = moviesJson[movieId].title;
+cardTitle.innerHTML = moviesJson[movieId].title;
 
-nativeValidation();
+cancelBtn.addEventListener('click', () => window.close());
+
+form.addEventListener('submit', event =>  nativeValidation(event));
 
 // functions
-function nativeValidation() {
-    form.addEventListener('submit', (event) => { // Validação nativa do formulário
-        event.preventDefault();
-        clearErrors(); 
-    
-        let send = true;
-        let check = checkInput(); 
-       
-        if(check !== true) { 
-            send = false;
-            showError(emailInput, check); 
-        }
-        
-        if(send) { 
-            localStorage.setItem("votes", `${localStorage.votes}-${movieId}#${voteInput.value}`);
-            opener.location.reload(); 
-            window.alert('Obrigado pelo seu voto!');
-            // form.submit();
-            window.close();            
-        }
-    });
+function nativeValidation(submitEvent) {
+    submitEvent.preventDefault();
+    clearErrors();
+
+    let send = true;
+    let check = checkInput();
+
+    if (check !== true) {
+        send = false;
+        showError(emailInput, check);
+    }
+
+    if (send) {
+        localStorage.setItem("votes", `${localStorage.votes}-${movieId}#${voteInput.value}`);
+        opener.location.reload();
+        window.alert('Obrigado pelo seu voto!');
+        // form.submit();
+        window.close();
+    }
 }
 
 function checkInput() { //verifica se o input foi preenchico corretamente
     let rules = emailInput.getAttribute('data-rules').split('|');
-    
-    for(i in rules) {
-        switch(rules[i]) {
+
+    for (i in rules) {
+        switch (rules[i]) {
             case 'required':
-                if(emailInput.value == '') {
+                if (emailInput.value == '') {
                     return 'Campo não pode ser vazio!';
                 }
-            break;
+                break;
 
             case 'email':
-                if(emailInput.value != '') {
+                if (emailInput.value != '') {
                     let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                    
-                    if(!regex.test(emailInput.value.toLowerCase())) {
+
+                    if (!regex.test(emailInput.value.toLowerCase())) {
                         return 'E-mail digitado não é válido!';
                     }
                 }
-            break;
+                break;
         }
     }
 
     return true;
 }
 
-function showError(input, error) { 
+function showError(input, error) {
     input.style.borderColor = '#FF0000';
 
     let errorElement = document.createElement('div');
@@ -73,11 +76,11 @@ function showError(input, error) {
     input.parentElement.insertBefore(errorElement, input.ElementSibling);
 }
 
-function clearErrors() { 
+function clearErrors() {
     let errorElements = document.querySelectorAll('.error');
     let errorQuantity = errorElements.length;
-    
-    for(i = 0 ; i < errorQuantity; i++) {
+
+    for (i = 0; i < errorQuantity; i++) {
         errorElements[i].remove();
     }
 }
